c4fbc0d89c79639a90017ce2edddfcd747e582de
Amanda <amandaduuaarte@gmail.com>
2022-02-24 21:04:24 -0300

    finalizando navbar

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  height: 40%;
+  height: 55%;
   width: 100%;
   padding: 24px;
 `;
---
+++ src/components/List/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const List = ({children}) =>{
+  return (
+    <Container>
+      {children}
+    </Container>
+  )
+}
+
+export default List;
