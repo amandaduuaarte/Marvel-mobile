079ed05e05cc6803511f739e4b41589fafc7ff56
Amanda <amandaduuaarte@gmail.com>
2022-03-08 20:02:02 -0300

    Fix: correção do eslint

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { 
-  Container, 
-  WelcomeText, 
-  InformationText, 
-  DivIcons, 
+import {
+  Container,
+  WelcomeText,
+  InformationText,
+  DivIcons,
   IconContainer,
 } from './style';
 
@@ -13,35 +13,30 @@ import Villain from '../../assets/images/icons/villain.svg';
 import AntiHero from '../../assets/images/icons/antihero.svg';
 import Human from '../../assets/images/icons/human.svg';
 
-
-const Header = () =>{
-  return(
+const Header = () => {
+  return (
     <Container>
       <WelcomeText>Bem vindo ao Marvel Heros</WelcomeText>
       <InformationText>Escolha o seu personagem</InformationText>
 
       <DivIcons>
         <IconContainer>
-        <Hero/>
+          <Hero />
         </IconContainer>
-        <IconContainer
-         background="#F2264B">
-           <Villain/>
+        <IconContainer background="#F2264B">
+          <Villain />
         </IconContainer>
-        <IconContainer
-        background="#B224EF">
-          <AntiHero/>
+        <IconContainer background="#B224EF">
+          <AntiHero />
         </IconContainer>
-        <IconContainer
-        background="#0BA360">
-          <Alien/>
+        <IconContainer background="#0BA360">
+          <Alien />
         </IconContainer>
-        <IconContainer
-        background="#FF7EB3">
-          <Human/>
+        <IconContainer background="#FF7EB3">
+          <Human />
         </IconContainer>
       </DivIcons>
     </Container>
-  )
-}
-export default Header
+  );
+};
+export default Header;
--- src/routes/user.stack.routes.tsx
+++ src/routes/user.stack.routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Home } from '../screens';
+import { Home, Character } from '../screens';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -8,6 +8,7 @@ export function UserStackRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="home" component={Home} />
+      <Screen name="Character" component={Character} />
     </Navigator>
   );
 }
